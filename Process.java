0e45c7ff2e8b193c8de174f29f37ad03dbb46244
Jerome Celeres <jeromeceleres@gmail.com>
2020-03-12 17:43:05 +0800

    Added performance parameter variables for processes in Process file

--- Process.java
+++ Process.java
@@ -1,11 +1,14 @@
 public class Process {
-    private int index, arrivalTime, burstTime, niceLevel;
+    private int index, arrivalTime, burstTime, niceLevel, waitingTime, turnaroundTime, responseTime;
 
     public Process(int index, int arrivalTime, int burstTime, int niceLevel) {
         this.index = index + 1;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
         this.niceLevel = niceLevel;
+        waitingTime = 0;
+        turnaroundTime = 0;
+        responseTime = 0;
     }
 
     public int getIndex() {
@@ -22,5 +25,29 @@ public class Process {
 
     public int getNiceTime() {
         return niceLevel;
+    }
+
+    public void updateWaitingTime(int n) {
+        waitingTime = n;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void updateTurnaroundTime(int n) {
+        turnaroundTime = n;
+    }
+
+    public int getTurnaroundTime() {
+        return turnaroundTime;
+    }
+
+    public void updateResponseTime(int n) {
+        responseTime = n;
+    }
+
+    public int getResponseTime() {
+        return responseTime;
     }
 }
