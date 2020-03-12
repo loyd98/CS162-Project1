641e0f505e90e0eb61134cdca7eeb19931520d44
Jerome Celeres <jeromeceleres@gmail.com>
2020-03-12 20:13:06 +0800

    Created the FCFS algorithm and made some minor edits throughout the Scheduler file

--- Scheduler.java
+++ Scheduler.java
@@ -1,16 +1,73 @@
+import java.lang.Math;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class Scheduler {
     private int testCases, numOfProcesses;
-    private ArrayList<Integer> timeQuantum = new ArrayList<>();
+    private ArrayList<Integer> timeQuantum;
+    private DecimalFormat df;
     private Process processList[][];
-    private Scanner input = new Scanner(System.in);
+    private Scanner input;
     private String[] schedulerAlgo, stdInput;
 
-    public Scheduler() {}
+
+    public Scheduler() {
+        timeQuantum = new ArrayList<>();
+        df = new DecimalFormat("0.0###");
+        df.setRoundingMode(RoundingMode.FLOOR);
+        input = new Scanner(System.in);
+    }
 
     // Scheduler Algorithms 
-    public void useFCFS(Process[] processList) {}
+    public void useFCFS(Process[] processList) {
+        int timeElapsed = 0;
+        int totalBurstTime = 0;
+        float totalWaitingTime = 0;
+        float totalTurnaroundTime = 0;
+        float totalResponseTime = 0;
+        Process[] arrivalTimeList = processList.clone();
+        bubbleSort(arrivalTimeList);
+        for(int i = 0; i < arrivalTimeList.length; i++) {
+            if(timeElapsed < arrivalTimeList[i].getArrivalTime()) {
+                timeElapsed = arrivalTimeList[i].getArrivalTime();
+            }
+            else if(timeElapsed > arrivalTimeList[i].getArrivalTime()) {
+                arrivalTimeList[i].updateWaitingTime(timeElapsed - arrivalTimeList[i].getArrivalTime());
+            }
+            arrivalTimeList[i].updateTurnaroundTime(arrivalTimeList[i].getWaitingTime() + arrivalTimeList[i].getBurstTime());
+            arrivalTimeList[i].updateResponseTime(timeElapsed - arrivalTimeList[i].getArrivalTime());
+            System.out.println(timeElapsed + " " + arrivalTimeList[i].getIndex() + " " + arrivalTimeList[i].getBurstTime() + "X");
+            timeElapsed += arrivalTimeList[i].getBurstTime();
+            totalBurstTime += arrivalTimeList[i].getBurstTime();
+        }
+
+        System.out.println("Total time elapsed: " + timeElapsed + "ns");
+        System.out.println("Total CPU burst time: " + totalBurstTime + "ns");
+        System.out.println("CPU Utilization: " + (int)Math.floor(((float) totalBurstTime / timeElapsed) * 100) + "%");
+        System.out.println("Throughput: " + (df.format((float)processList.length / timeElapsed)) + " processes/ns");
+
+        System.out.println("Waiting times:");
+        for(int i = 0; i < processList.length; i++) {
+            System.out.println(" Process " + processList[i].getIndex() + ": " + processList[i].getWaitingTime() + "ns");
+            totalWaitingTime += processList[i].getWaitingTime();
+        }
+        System.out.println("Average waiting time: " + df.format(totalWaitingTime / processList.length) + "ns");
+
+        System.out.println("Turnaround times:");
+        for(int i = 0; i < processList.length; i++) {
+            System.out.println(" Process " + processList[i].getIndex() + ": " + processList[i].getTurnaroundTime() + "ns");
+            totalTurnaroundTime += processList[i].getTurnaroundTime();
+        }
+        System.out.println("Average turnaround time: " + df.format(totalTurnaroundTime / processList.length) + "ns");
+
+        System.out.println("Response times:");
+        for(int i = 0; i < processList.length; i++) {
+            System.out.println(" Process " + processList[i].getIndex() + ": " + processList[i].getResponseTime() + "ns");
+            totalResponseTime += processList[i].getResponseTime();
+        }
+        System.out.println("Average response time: " + df.format(totalResponseTime / processList.length) + "ns");
+    }
 
     public void useSJF(Process[] processList) {}
 
@@ -57,31 +114,28 @@ public class Scheduler {
                 int niceLevel = input.nextInt();
                 processList[i][j] = new Process(j, arrivalTime, burstTime, niceLevel);
             }
-            bubbleSort(processList[i]);
         } 
         
         System.out.println("\n" + "-----OUTPUT-----");
         for(int i = 0; i < processList.length; i++) {
             System.out.println((i + 1) + " " + schedulerAlgo[i]);
-            for(int j = 0; j < processList[i].length; j++) {
 
-                if(schedulerAlgo[i].equals("FCFS")) {
-                    useFCFS(processList[i]);
-                }
-                else if(schedulerAlgo[i].equals("SJF")) {
-                    useSJF(processList[i]);
-                }
-                else if(schedulerAlgo[i].equals("SRTF")) {
-                    useSRTF(processList[i]);
-                }
-                else if(schedulerAlgo[i].equals("P")) {
-                    useP(processList[i]);
-                }
-                else if(schedulerAlgo[i].equals("RR")) {
-                    int q = timeQuantum.get(0);
-                    timeQuantum.remove(0);
-                    useRR(processList[i], q);
-                }
+            if(schedulerAlgo[i].equals("FCFS")) {
+                useFCFS(processList[i]);
+            }
+            else if(schedulerAlgo[i].equals("SJF")) {
+                useSJF(processList[i]);
+            }
+            else if(schedulerAlgo[i].equals("SRTF")) {
+                useSRTF(processList[i]);
+            }
+            else if(schedulerAlgo[i].equals("P")) {
+                useP(processList[i]);
+            }
+            else if(schedulerAlgo[i].equals("RR")) {
+                int q = timeQuantum.get(0);
+                timeQuantum.remove(0);
+                useRR(processList[i], q);
             }
         }
     }
