cd9e4a398517cc462b00a08b00a52a6e8e84b5d1
Jerome Celeres <jeromeceleres@gmail.com>
2020-03-13 15:05:26 +0800

    Created the SJF algorithm

--- Scheduler.java
+++ Scheduler.java
@@ -7,8 +7,10 @@ public class Scheduler {
     private int testCases, numOfProcesses;
     private ArrayList<Integer> timeQuantum;
     private DecimalFormat df;
+
     // First [] indicates the test case while the second [] stores the processes of said test case.
     private Process processList[][];
+
     private Scanner input;
     private String[] schedulerAlgo, stdInput;
 
@@ -157,7 +159,59 @@ public class Scheduler {
     }
 
     // Shortest Job First (non-preemptive) Algorithm 
-    public void useSJF(Process[] processList) {}
+    public void useSJF(Process[] processList) {
+        int timeElapsed = 0;
+        int totalBurstTime = 0;
+
+        /* 
+        Clones the processList array so that it is arranged by arrival time.
+        Copies the arranged array into an array list.
+        This array list will handle which process will run first. 
+        It is a shallow copy of processList.
+        */
+        Process[] arrivalTimeList = processList.clone();
+        ArrayList<Process> processQueue = new ArrayList<>(Arrays.asList(bubbleSort(arrivalTimeList))); 
+
+        while(!processQueue.isEmpty()) {
+            if(timeElapsed < processQueue.get(0).getArrivalTime()) {
+                timeElapsed = processQueue.get(0).getArrivalTime();
+            }
+
+            if(timeElapsed >= processQueue.get(0).getArrivalTime()) {
+
+                // Modified bubble sort algorithm
+                for(int i = 0; i < processQueue.size(); i++) {
+                    for(int j = 0; j < processQueue.size() - 1 - i; j++) { 
+                        // Checks if the current time encompasses currently selected process
+                        // If true then compare burst times.
+                        if (timeElapsed >= processQueue.get(j + 1).getArrivalTime()) {
+                            if(processQueue.get(j).getBurstTime() > processQueue.get(j + 1).getBurstTime()) {
+                                Process temp = processQueue.get(j);
+                                processQueue.set(j, processQueue.get(j + 1));
+                                processQueue.set(j + 1, temp);
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Updates the performance parameters of current process
+            processQueue.get(0).updateWaitingTime(timeElapsed - processQueue.get(0).getArrivalTime());
+            processQueue.get(0).updateTurnaroundTime(processQueue.get(0).getWaitingTime() + processQueue.get(0).getBurstTime());
+            processQueue.get(0).updateResponseTime(timeElapsed - processQueue.get(0).getArrivalTime());
+
+            // Output of SJF Algorithm per processor
+            System.out.println(timeElapsed + " " + processQueue.get(0).getIndex() + " " + processQueue.get(0).getBurstTime() + "X");
+
+            // Keeps track of the time elapsed and total burst time. To be used for analysis portion of data output.
+            timeElapsed += processQueue.get(0).getBurstTime();
+            totalBurstTime += processQueue.get(0).getBurstTime();
+            processQueue.remove(0);
+        }
+
+        // SJF Data Analysis
+        analyzePerformance(processList, timeElapsed, totalBurstTime);
+    } 
 
     // Shortest Remaining Time First (preemptive) Algorithm 
     public void useSRTF(Process[] processList) {}
